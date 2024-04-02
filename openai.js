87c1c3f5b9dce79d21aa480fc3a0830a344acca1
Thomas DC <thomas.dicola@hotmail.fr>
2024-04-02 08:50:45 +0200

    done

--- openai.js
+++ openai.js
@@ -8,7 +8,7 @@ const openai = new OpenAI({
 async function main() {
   const image = await openai.images.generate({
     model: 'dall-e-2',
-    prompt: 'baby black cat playing with a basket ball',
+    prompt: 'a baby black cat playing with a ball',
   });
   open(image.data[0].url);
 }
